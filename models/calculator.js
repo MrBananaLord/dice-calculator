3199df78e57b7782d51204917f08b729214ce6c2
jan-bator <jan.bator@xing.com>
2020-03-04 11:04:35 +0100

    Strip down to simple JS+HTML+CSS

--- models/calculator.js
+++ models/calculator.js
@@ -1,88 +1,84 @@
 class Calculator {
-  constructor() {
-    insertHTML(`INSERT(templates/calculator.html)`);
-    insertCSS(`INSERT(styles/calculator.css)`);
+    constructor() {
+        this.element = $("#rollingStonesCalculator");
+        this.element.click(e => this.toggle(e));
+        this.element.find(".box div").click(e => e.stopPropagation());
+        this.element.find(".close").click(e => this.toggle(e));
+        this.element.find(".reroll").click(e => this.reroll(e));
+        this.element.find(".key").click(e => this.keyClick(e));
 
-    this.element = $("#rollingStonesCalculator");
-    this.element.click(e => this.toggle(e));
-    this.element.find(".box div").click(e => e.stopPropagation());
-    this.element.find(".close").click(e => this.toggle(e));
-    this.element.find(".reroll").click(e => this.reroll(e));
-    this.element.find(".key").click(e => this.keyClick(e));
+        this.displayElement = this.element.find(".display .value");
+        this.resultElement = this.element.find(".display .result");
 
-    this.displayElement = this.element.find(".display .value");
-    this.resultElement = this.element.find(".display .result");
+        this.equasion = new Equasion();
+        this.mode = "input";
+    }
 
-    this.equasion = new Equasion();
-    this.mode = "input";
-  }
+    keyClick(e) {
+        let target = $(e.target);
+        let action = target.data("action");
 
-  keyClick(e) {
-    let target = $(e.target);
-    let action = target.data("action");
+        if (action == null) {
+            if (this.mode == "result") {
+                this.inputMode();
+            }
 
-    if (action == null) {
-      if (this.mode == "result") {
+            this.push(target.data("value"));
+        } else {
+            this[action]();
+        }
+
+        this.updateEquasionDisplay();
+
+        e.stopPropagation();
+    }
+
+    inputMode() {
+        this.mode = "input";
+        this.equasion.reset();
+        this.resultElement.addClass("hidden");
+        this.resultElement.find(".regular").text();
+        this.updateEquasionDisplay()
+    }
+
+    push(value) {
+        this.equasion.addCharacter(value);
+    }
+
+    updateEquasionDisplay() {
+        if (!this.equasion.valid) {
+            this.element.addClass('invalid');
+        } else {
+            this.element.removeClass('invalid');
+        }
+
+        this.displayElement.text(this.equasion.tokens.map((t) => t.value).join(""));
+    }
+
+    calculate() {
+        let result = this.equasion.result;
+
+        this.updateResults(result);
+        this.mode = "result";
+    }
+
+    clear() {
         this.inputMode();
-      }
-
-      this.push(target.data("value"));
-    } else {
-      this[action]();
     }
 
-    this.updateEquasionDisplay();
-
-    e.stopPropagation();
-  }
-
-  inputMode() {
-    this.mode = "input";
-    this.equasion.reset();
-    this.resultElement.addClass("hidden");
-    this.resultElement.find(".regular").text();
-    this.updateEquasionDisplay()
-  }
-
-  push(value) {
-    this.equasion.addCharacter(value);
-  }
-
-  updateEquasionDisplay() {
-    if (!this.equasion.valid) {
-      this.element.addClass('invalid');
-    }
-    else {
-      this.element.removeClass('invalid');
+    revert() {
+        if (this.mode == "input") {
+            this.equasion.tokens.pop();
+        }
     }
 
-    this.displayElement.text(this.equasion.tokens.map((t) => t.value).join(""));
-  }
-
-  calculate() {
-    let result = this.equasion.result;
-
-    this.updateResults(result);
-    this.mode = "result";
-  }
-
-  clear() {
-    this.inputMode();
-  }
-
-  revert() {
-    if (this.mode == "input") {
-      this.equasion.tokens.pop();
+    updateResults(roll) {
+        this.resultElement.removeClass("hidden");
+        this.resultElement.find(".regular").text(roll);
     }
-  }
 
-  updateResults(roll) {
-    this.resultElement.removeClass("hidden");
-    this.resultElement.find(".regular").text(roll);
-  }
-
-  toggle(e) {
-    this.element.toggleClass("hidden");
-    e.preventDefault();
-  }
+    toggle(e) {
+        this.element.toggleClass("hidden");
+        e.preventDefault();
+    }
 }
