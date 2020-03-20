60a8c2d7308e8d76c5a8f72c368f7f7a482b6c33
jan-bator <jan.bator@xing.com>
2020-03-20 18:56:42 +0100

    Add some more style.

--- models/history.js
+++ models/history.js
@@ -1,10 +1,15 @@
 class History {
     constructor() {
         this.displayElement = $("#rollingStonesCalculator .history");
+        this.toggleKey = $("#rollingStonesCalculator .key[data-action='showHistory']");
         this.storage = window.localStorage;
 
         this.elements = JSON.parse(this.storage["history"]) || [];
         this.elements.forEach((e) => this.displayElement.append(`<div>${e}</div>`));
+
+        if (this.elements.length > 0) {
+            this.toggleKey.removeClass("disabled");
+        }
     }
 
     push(equasion) {
